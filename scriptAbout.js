27ac63c9a2d07876af8c43f5a55c0735965afe22
unknown <kashif_saleem_23@hotmail.com>
2022-11-24 19:01:15 +0500

    Add all files and folders for the capstone project

---
+++ scriptAbout.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.desktop-navigation');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  header.classList.toggle('full-screen');
+  body.classList.toggle('no-scroll');
+});
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  header.classList.remove('full-screen');
+  body.classList.remove('no-scroll');
+}));
